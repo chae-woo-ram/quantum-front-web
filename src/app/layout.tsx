c4bd680a6c44c92f01d868464f97fd93e3dc8fa6
이채은 <loev1021@gmail.com>
2024-07-23 17:55:33 +0900

    mui 셋팅 및 테이블, 모달 컴포넌트 개발

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Noto_Sans_KR } from 'next/font/google';
 import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
 import Providers from '@/lib/Provider';
+import theme from '@/styles/theme';
+import { ThemeProvider } from '@mui/material/styles';
 import styles from './page.module.css';
 
 export const metadata: Metadata = {
@@ -21,15 +23,17 @@ const notoSansKr = Noto_Sans_KR({
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body className={notoSansKr.className}>
-        <Providers>
-          <div className={styles.container}>
-            <Header />
-            <main className={styles.main}>{children}</main>
-            <Footer />
-          </div>
-        </Providers>
-      </body>
+      <ThemeProvider theme={theme}>
+        <body className={notoSansKr.className}>
+          <Providers>
+            <div className={styles.container}>
+              <Header />
+              <main className={styles.main}>{children}</main>
+              <Footer />
+            </div>
+          </Providers>
+        </body>
+      </ThemeProvider>
     </html>
   );
 };
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,18 @@
-const theme = {
-  colors: {
-    color1: '#000000',
-    color2: '#111111',
+'use client';
+
+import { Roboto } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: roboto.style.fontFamily,
   },
-};
+});
 
 export default theme;
