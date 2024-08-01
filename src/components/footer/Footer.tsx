5f2ed7a0fd38a9c2376e82501c0f6b9d672889a1
정보람 <boram33377@gmail.com>
2024-08-01 13:11:05 +0900

    [feat] footer 작업

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ import { styled } from 'styled-components';
 export const Footer = () => {
   return (
     <FooterContainer>
-      <p>&copy; 2024 My App. All rights reserved.</p>
+      <p>&copy; 2024 CHAEWOORAM GALLERY. All Rights Reserved. </p>
     </FooterContainer>
   );
 };
@@ -13,4 +13,8 @@ export const Footer = () => {
 const FooterContainer = styled.footer`
   padding: 1rem;
   text-align: center;
+  font-size: 10px;
+  height: 48px;
+  background-color: #e8e8e8;
+  color: #a0a0a0;
 `;
