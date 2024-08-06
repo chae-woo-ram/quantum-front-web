f71cb200cdc22858bd2386a12f1d465cd8575aa5
이채은 <loev1021@gmail.com>
2024-08-06 18:47:10 +0900

    feat: 메인 배너 UI 변경 및 애니메이션 적용

--- src/components/banner/Banner3.tsx
+++ src/components/banner/Banner3.tsx
@@ -1,96 +1,108 @@
 'use client';
 
+import { useRef } from 'react';
 import { useRouter } from 'next/navigation';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { Button } from '@mui/material';
+import { motion, useInView } from 'framer-motion';
 import styled from 'styled-components';
 
 export const Banner3 = () => {
   const router = useRouter();
+  const ref = useRef(null);
+
+  const inView = useInView(ref, {
+    once: false,
+  });
+
+  const getAnimationProps = (index) => {
+    return {
+      opacity: inView ? 1 : 0,
+      y: inView ? 0 : 30,
+      transition: { duration: 1, delay: index * 0.5 },
+    };
+  };
 
   return (
     <>
       <Banner3Container>
-        <Title>
-          <p>
+        <TextContent>
+          <AnimatedTitle ref={ref} initial={{ opacity: 0, y: 30 }} animate={getAnimationProps(0)}>
             GALLERY MARKETPLACE: <span>실시간 채팅으로 예술 거래를 새롭게</span>
-          </p>
-          <p>"예술 작품을 사고파는 새로운 방식을 경험해보세요."</p>
-          <p>
-            우리 플랫폼은 아티스트와 직접 연결되어 실시간 채팅을 통해 원활한 소통과 거래를 가능하게 합니다.
+          </AnimatedTitle>
+          <AnimatedSubtitle ref={ref} initial={{ opacity: 0, y: 30 }} animate={getAnimationProps(1)}>
+            "예술 작품을 사고파는 새로운 방식을 경험해보세요."
+          </AnimatedSubtitle>
+          <AnimatedDescription ref={ref} initial={{ opacity: 0, y: 30 }} animate={getAnimationProps(2)}>
+            우리 플랫폼은 아티스트와 직접 연결되어 <br /> 실시간 채팅을 통해 원활한 소통과 거래를 가능하게 합니다.
             <br />전 세계의 독창적인 작품을 발견하고, 창작자들과 의미 있는 관계를 구축해보세요.
-          </p>
-        </Title>
+          </AnimatedDescription>
+          <ChatButtonWrapper>
+            <ChatButton
+              variant="contained"
+              color="secondary"
+              size="small"
+              onClick={() => router.push(`/Chats`)}
+              startIcon={<ArrowForwardIcon />}
+            >
+              Chat 시작하기
+            </ChatButton>
+          </ChatButtonWrapper>
+        </TextContent>
 
         <VideoWrapper>
           <Video autoPlay loop muted>
             <source src="/video/chatVideo.mp4" type="video/mp4" />
           </Video>
         </VideoWrapper>
-
-        <ChatButtonWrapper>
-          <ChatButton
-            variant="contained"
-            color="secondary"
-            size="small"
-            onClick={() => router.push(`/Chats`)}
-            startIcon={<ArrowForwardIcon />}
-          >
-            Chat 시작하기
-          </ChatButton>
-        </ChatButtonWrapper>
       </Banner3Container>
     </>
   );
 };
 
 const Banner3Container = styled.div`
-  padding: 80px 10%;
-  background: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);
   display: flex;
-  flex-direction: column;
   align-items: center;
+  margin: 0 auto;
 `;
 
-const Title = styled.h1`
-  color: #2a2a2a;
-  font-size: 18px;
+const TextContent = styled.div`
+  width: 50%;
+  height: 1200px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   line-height: 1.5;
-  text-align: center;
-  max-width: 800px;
-  margin-bottom: 40px;
+  background: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);
+  padding: 20px; /* Added padding for better spacing */
+`;
 
-  p:first-child {
-    font-size: 22px;
-    font-weight: bold;
-    margin-bottom: 20px;
+const AnimatedTitle = styled(motion.h1)`
+  color: #2a2a2a;
+  font-size: 22px;
+  font-weight: bold;
+  margin-bottom: 20px;
 
-    > span {
-      font-weight: normal;
-      font-size: 20px;
-    }
+  & span {
+    font-weight: normal;
+    font-size: 20px;
   }
+`;
 
-  p:nth-child(2) {
-    font-size: 30px;
-    font-weight: bold;
-    position: relative;
-    padding: 10px 20px;
-    border-radius: 8px;
-    color: transparent;
-
-    background: linear-gradient(135deg, #c3cfe2, #c7d7f0, #e0aaff, #b09adb, #cfc4e0);
-    background-clip: text;
-    -webkit-background-clip: text;
-
-    border: 2px solid transparent;
-    background-size: 300% 300%;
-    animation: gradientAnimation 7s ease infinite;
-  }
+const AnimatedSubtitle = styled(motion.p)`
+  font-size: 28px;
+  font-weight: bold;
+  position: relative;
+  padding: 10px 20px;
+  border-radius: 8px;
+  color: transparent;
+  background: linear-gradient(135deg, #c3cfe2, #c7d7f0, #e0aaff, #b09adb, #cfc4e0);
+  background-clip: text;
+  -webkit-background-clip: text;
+  border: 2px solid transparent;
+  background-size: 300% 300%;
+  animation: gradientAnimation 7s ease infinite;
 
   @keyframes gradientAnimation {
     0% {
@@ -105,28 +117,41 @@ const Title = styled.h1`
   }
 `;
 
+const AnimatedDescription = styled(motion.p)`
+  color: #2a2a2a;
+  font-size: 18px;
+  text-align: right;
+  line-height: 1.5;
+`;
+
 const VideoWrapper = styled.div`
-  width: 100%;
+  width: calc(100vw / 2);
+  height: 1200px;
   display: flex;
   justify-content: center;
-  margin-top: 20px;
+  background-size: auto;
+  background-position: center;
+  background-image: url('/images/main_banner_bg.png');
+  position: relative;
 `;
 
 const Video = styled.video`
-  width: 80%;
-  max-width: 800px;
+  width: 450px;
   height: auto;
-  border-radius: 10px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  position: absolute;
+  top: 53%;
+  left: 52%;
+  transform: translate(-50%, -50%);
 
   @media (max-width: 768px) {
     width: 100%;
+    left: 50%;
+    transform: translateX(-50%);
   }
 `;
 
 const ChatButtonWrapper = styled.div`
-  text-align: center;
-  margin-top: 30px;
+  margin-top: 50px;
 `;
 
 const ChatButton = styled(Button)`
