8ca03856855090f7fdd921657c10c86bcfff2a7a
이채은 <loev1021@gmail.com>
2024-08-19 11:02:14 +0900

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,18 @@ import { type NextRequest } from 'next/server';
 import { updateSession } from '@/app/utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
+  // 세션 업데이트 함수 호출
   return await updateSession(request);
 }
 
 export const config = {
   matcher: [
     /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
+     * 다음 경로들은 제외하고 모든 요청 경로를 매칭합니다:
+     * - _next/static (정적 파일)
+     * - _next/image (이미지 최적화 파일)
+     * - favicon.ico (파비콘 파일)
+     * - 이미지 파일 (svg, png, jpg, jpeg, gif, webp)
      */
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
